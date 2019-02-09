6ba38afd80238df957c44845b0f0b82559d81167
Masashi Fujita <objectxtreme@gmail.com>
2019-02-09 23:10:39 +0900

    refactor: Make parser parametric

--- src/parse-nmake.cpp
+++ src/parse-nmake.cpp
@@ -47,47 +47,6 @@ namespace {
 
 namespace DependencyFileParser {
     Result ParseNMakeStyle (const char* src, size_t size) {
-        auto const end_p = src + size;
-        src = skip_comment_and_space (src, end_p);
-        if (src == end_p) {
-            // Empty definitions.
-            return {};
-        }
-        // 1st: Obtains target.
-        auto const &target = fetch_token (src, end_p);
-        src = std::get<1> (target);
-        if (src == end_p) {
-            // Malformed dependency definition.
-            return {};
-        }
-        std::string target_path ;
-        // Handle "<filepath>:" case.
-        {
-            auto const &p = std::get<0> (target);
-            if (*std::rbegin (p) == ':') {
-                target_path = p.substr (0, p.size () - 1);
-            }
-            else {
-                target_path = std::get<0> (target);
-                if (*src != ':') {
-                    return {};  // Malformed.
-                }
-            }
-        }
-        src = skip_space (src + 1, end_p);
-        if (src == end_p) {
-            return {};
-        }
-        // 2nd: Obtains dependents.
-        std::vector<std::string>    deps;
-        while (src < end_p) {
-            auto dep = fetch_token (src, end_p);
-            deps.emplace_back (std::get<0> (dep));
-            src = std::get<1> (dep);
-            if (src == end_p) {
-                break;
-            }
-        }
-        return {std::move (target_path), std::move (deps)};
+        return parse (fetch_token, src, src + size);
     }
 } // namespace DependencyFileParser
--- src/utility.hpp
+++ src/utility.hpp
@@ -5,7 +5,14 @@
 #ifndef utility_hpp__779FE0AC_DF0D_4F24_9312_AB0C307D20B9
 #define utility_hpp__779FE0AC_DF0D_4F24_9312_AB0C307D20B9
 
+#include <depfile-parser.hpp>
+#include <memory>
+#include <string>
+#include <tuple>
+#include <vector>
+
 namespace DependencyFileParser {
+
     namespace detail {
         /// @brief Skips white spaces
         /// @param src Start of the sequence
@@ -26,6 +33,62 @@ namespace DependencyFileParser {
         ///         Otherwise return `end_p`.
         const char* skip_comment_and_space (const char* p, const char* end_p);
 
+        /// @brief Parse a dependency description.
+        ///
+        ///        Assumes the following inputs:<br/>
+        ///        &lt;target&gt;:&lt;primary prerequisite&gt; [&lt;prerequisites...&gt;]
+        /// @tparam Tokenizer_ Type of the tokenizer
+        /// @param tok The tokenizer
+        /// @param src Start of the sequence
+        /// @param end_p End of the sequence
+        /// @return Dependency information.
+        /// @remarks Tokenizer_ should satisfy `std::is_invokable_r_v<const char *, const char *, const char *>`.
+        template <typename Tokenizer_>
+            ::DependencyFileParser::Result parse (Tokenizer_ tok, const char* src, const char* end_p) {
+                src = skip_comment_and_space (src, end_p);
+                if (src == end_p) {
+                    // Empty definitions.
+                    return {};
+                }
+                // 1st: Obtains target.
+                auto const& target = tok (src, end_p);
+
+                src = std::get<1> (target);
+                if (src == end_p) {
+                    // Malformed dependency definition.
+                    return {};
+                }
+                std::string target_path;
+                // Handle "<filepath>:" case.
+                {
+                    auto const& p = std::get<0> (target);
+                    if (*std::rbegin (p) == ':') {
+                        // Adhered ':'
+                        target_path = p.substr (0, p.size () - 1);
+                    }
+                    else {
+                        target_path = std::get<0> (target);
+                        if (*src != ':') {
+                            return {}; // Malformed.
+                        }
+                    }
+                }
+                src = skip_space (src + 1, end_p);
+                if (src == end_p) {
+                    return {};
+                }
+                // 2nd: Obtains dependents.
+                std::vector<std::string> deps;
+                while (src < end_p) {
+                    auto dep = tok (src, end_p);
+                    deps.emplace_back (std::get<0> (dep));
+                    src = std::get<1> (dep);
+                    if (src == end_p) {
+                        break;
+                    }
+                }
+                return {std::move (target_path), std::move (deps)};
+            }
     } // namespace detail
 } // namespace DependencyFileParser
 
